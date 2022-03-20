924212e8cb6ec7736fdef1ae7ff5fe5a5ae11c6e
Krintox <87925419+Krintox@users.noreply.github.com>
2022-03-20 20:46:48 +0530

    Added the hover effect on images

---
+++ Scripts/index.js
@@ -0,0 +1,7 @@
+
+
+function nextCard() {
+    document.getElementsByClassName('junior-core-carousel').style.color = "green";
+    document.getElementsByClassName('junior-cofe-carousel-2').style.display = "grid";
+    decodeURIComponent.getElementsByClassName('judges-heading').style.color = "green";
+}
